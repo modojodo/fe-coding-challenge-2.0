b2b6436ccb8a2a578d1a902c1e8b78b646092335
Umer Hassan <umerhassan94@gmail.com>
2021-01-24 00:59:49 -0500

    improvement(store): replace the variables with Vue interpolation before pushing nodes into the state

--- src/store.js
+++ src/store.js
@@ -8,10 +8,30 @@ const store = new Vuex.Store({
     survey: {
       "name": "COVID-19 self checker",
     },
-    activeNodeId: null
+    activeNodeId: null,
   },
   actions: {
     updateNodes({ commit }, newNodes) {
+      // Get list of all variables
+      const variables = [];
+      for (const [, val] of Object.entries(newNodes)) {
+        if (val.variable) {
+          variables.push(val.variable);
+        }
+      }
+      // Replace variables with Vue interpolation
+      for (const [key, val] of Object.entries(newNodes)) {
+        variables.forEach(variable => {
+          if (val.content && val.content.includes(variable)) {
+            newNodes[key].content = val.content.replace(`#${variable}#`, `{{${variable}}}`)
+          }
+
+          if (val.page_title && val.page_title.includes(variable)) {
+            newNodes[key].page_title = val.page_title.replace(`#${variable}#`, `{{${variable}}}`)
+          }
+        });
+      }
+
       // Update nodes with new data from the API
       commit('UPDATE_NODES', newNodes);
       // Set active node to 1 whenever data updates
